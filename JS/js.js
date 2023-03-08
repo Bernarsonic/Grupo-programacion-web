c7f7ba656f8bb53c2a0f7ae38277365bc8f85973
AntonioJMaqueda <antoniojosemaqueda82@gmail.com>
2023-03-08 09:56:59 +0100

    algo de css y ordenamiento

---
+++ JS/js.js
@@ -0,0 +1,53 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+
+
+    function coger_datos() {
+        // Captura de valores
+        const titulo = document.getElementById("titulo").value;
+        const descripcion = document.getElementById("descripcion").value;
+        const link = document.getElementById("url").value;
+        const autor = document.getElementById("autor").value;
+        const categoria = document.getElementById("categoria").value;
+        const fecha = document.getElementById("fecha").value;
+
+
+// Obtener los datos existentes del localStorage
+        let datosAnteriores = JSON.parse(localStorage.getItem('cadenaJSON')) || [];
+
+        //Aqui se meten los datos
+        const noticia ={
+            titulo: titulo,
+            descripcion: descripcion,
+            link: link,
+            autor: autor,
+            categoria: categoria,
+            fecha: fecha,
+        };
+        datosAnteriores.push(noticia);
+
+
+// Guardar los datos actualizados en el localStorage
+    localStorage.setItem('cadenaJSON', JSON.stringify(datosAnteriores));
+
+
+        //imprime en la consola
+        console.log(noticia)
+    }
+
+    function borrar() {
+        const titulo = document.getElementById("titulo").value = "";
+        const descripcion = document.getElementById("descripcion").value = "";
+        const link = document.getElementById("url").value = "";
+        const autor = document.getElementById("autor").value = "";
+        const categoria = document.getElementById("categoria").value = "";
+        const fecha = document.getElementById("fecha").value = "";
+    }
+
+    const botonSumit = document.getElementById('BotonS');
+    botonSumit.addEventListener('click', coger_datos);
+
+    const botonVaciar = document.getElementById("BotonV");
+    botonVaciar.addEventListener('click', borrar);
+
+});
