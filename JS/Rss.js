06f943ce1c63c86f9b20b76cb16d16c58a81403b
Bernarsonic <104491315+Bernarsonic@users.noreply.github.com>
2023-03-08 21:59:01 +0100

    Add files via upload

--- JS/Rss.js
+++ JS/Rss.js
@@ -1,4 +1,5 @@
 const generarRSS = () => {
+    const items = JSON.parse(localStorage.getItem('cadenaJSON')).slice(-5).reverse();
     let rss = '<?xml version="1.0" encoding="UTF-8" ?>\n';
     rss += '<rss version="2.0">\n';
     rss += '<channel>\n';
@@ -8,27 +9,16 @@ const generarRSS = () => {
     rss += '<date>Fecha</date>\n';
     rss += '<description>Noticias de ejemplo</description>\n';
 
-
-    for (let i = 0; i < localStorage.length; i++) {
-        const clave = localStorage.key(i);
-        if (clave.startsWith("datos")) {
-
-            const datosJSON = localStorage.getItem(clave);
-            const datos = JSON.parse(datosJSON);
-            const titulo = datos.Titulo;
-            const url = datos.URL; // reemplaza 'URL' con el nombre de la clave donde guardas la URL de la noticia
-
-            rss += '<item>\n';
-            rss += '<title>${titulo}</title>\n';
-            rss += '<link>${url}</link>\n';
-            rss += '<author>${datos.Autor}</author>\n';
-            rss += '<category>${datos.Categoría}</category>\n';
-            rss += '<date>${datos.Fecha}</date>\n';
-            rss += '<description>${datos.Descripción}</description>\n';
-
-            rss += '</item>\n';
-        }
-    }
+    items.forEach((item) => {
+        rss += '<item>\n';
+        rss += `<title>${item.titulo}</title>\n`;
+        rss += `<descripcion>${item.descripcion}</descripcion>\n`;
+        rss += `<link>${item.link}</link>\n`;
+        rss += `<autor>${item.autor}</autor>\n`;
+        rss += `<categoria>${item.categoria}</categoria>\n`;
+        rss += `<date>${item.fecha}</date>\n`;
+        rss += '</item>\n';
+    });
 
     rss += '</channel>\n';
     rss += '</rss>';
@@ -37,7 +27,7 @@ const generarRSS = () => {
     const url = URL.createObjectURL(blob);
     const enlace = document.createElement('a');
     enlace.href = url;
-    enlace.download = 'noticias.rss';
+    enlace.download = 'noticias.xml';
     document.body.appendChild(enlace);
     enlace.click();
     document.body.removeChild(enlace);
