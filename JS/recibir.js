0e53643c14764023b2f88f59f89978db14dd9c9c
AntonioJMaqueda <114919854+AntonioJMaqueda@users.noreply.github.com>
2023-03-08 20:24:14 +0100

    me quiero matar

--- JS/recibir.js
+++ JS/recibir.js
@@ -9,47 +9,38 @@ let html = '';
 var enlace;
 
 
-for(var a=0; a< 5; a++){
-    console.log(datos);
-    // Escribo el "html" personalizado
-
-
-
-  if (a===0) {
-
-      enlace = "noticia1.html"
-
-  } else if (a===1) {
-
-      enlace = "noticia2.html"
-
-  } else if (a===2) {
-
-      enlace = "noticia3.html"
-
-  } else if (a===3) {
-
-      enlace = "noticia4.html"
-
-  } else if (a===4) {
-
-      enlace = "noticia5.html"
-
-  }
-
-
-
-
-  }
 for(var b=0; b< datos.length; b++){
+    if (b===0) {
+
+        enlace = "Noticia1.html"
+
+    } else if (b===1) {
+
+        enlace = "Noticia2.html"
+
+    } else if (b===2) {
+
+        enlace = "Noticia3.html"
+
+    } else if (b===3) {
+
+        enlace = "Noticia4.html"
+
+    } else if (b===4) {
+
+        enlace = "Noticia5.html"
+
+    }
     // Escribo el "html" personalizado
     html = html + `
   <br>
+  <ol>
   <li><h1><a href="${enlace}">${datos[b].titulo}</a></h1></li>
   <li><b>Fecha:</b> ${datos[b].fecha}</li>  
   <li><b>Categoría:</b> ${datos[b].categoria}</li>
   <li><b>Descripción:</b> ${datos[b].descripcion}</li>
   <li><b>Autor:</b> ${datos[b].autor}</li>
+  </ol>
   <br>
   `
     ;
