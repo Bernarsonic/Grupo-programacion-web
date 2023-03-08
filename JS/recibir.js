f5c3c559e7fc2eda8b2c86219207a4030037c269
Bernarsonic <104491315+Bernarsonic@users.noreply.github.com>
2023-03-08 21:23:49 +0100

    final?

--- JS/recibir.js
+++ JS/recibir.js
@@ -5,20 +5,47 @@ const datos = JSON.parse(dat) || []; // En caso de que no haya datos, se estable
 // Obtener la referencia al contenedor de datos en la página
 const datosContainer = document.getElementById('datos-container');
 
-// Crear una plantilla de cadena HTML para mostrar los datos
-const datosHTML = `
-  <h2>Últimas 5 noticias:</h2>
-  <ul>
-    ${datos.slice(-5).map(dato => `
-    <br>
-    <li><b>Fecha:</b> ${dato.fecha}</li>  
-    <li><b>Categoria:</b> ${dato.categoria}</li>
-    <li><b>Descripcion:</b> ${dato.descripcion}</li>
-    <li><b>Autor:</b> ${dato.autor}</li>
-    <li><b>Titulo:</b> ${dato.titulo}</li>
-    `).join('')}
-  </ul>
-`;
+let html = '';
+var enlace;
+
+
+for(var b=0; b< datos.length; b++){
+    if (b===0) {
+
+        enlace = "Noticia1.html"
+
+    } else if (b===1) {
+
+        enlace = "Noticia2.html"
+
+    } else if (b===2) {
+
+        enlace = "Noticia3.html"
+
+    } else if (b===3) {
+
+        enlace = "Noticia4.html"
+
+    } else if (b===4) {
+
+        enlace = "Noticia5.html"
+
+    }
+    // Escribo el "html" personalizado
+    html = html + `
+  <br>
+  <ol>
+  <li><h1><a href="${enlace}">${datos[b].titulo}</a></h1></li>
+  <li><b>Fecha:</b> ${datos[b].fecha}</li>  
+  <li><b>Categoría:</b> ${datos[b].categoria}</li>
+  <li><b>Descripción:</b> ${datos[b].descripcion}</li>
+  <li><b>Autor:</b> ${datos[b].autor}</li>
+  </ol>
+  <br>
+  `
+    ;
+}
+
 
 // Agregar la plantilla al contenedor de datos
-datosContainer.innerHTML = datosHTML;
+datosContainer.innerHTML = html;
