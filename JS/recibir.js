e7e939b8868dd5ead2557640da7ec71a902d1de7
Bernarsonic <104491315+Bernarsonic@users.noreply.github.com>
2023-03-08 14:29:43 +0100

    Add files via upload

--- JS/recibir.js
+++ JS/recibir.js
@@ -5,17 +5,20 @@ const datos = JSON.parse(dat) || []; // En caso de que no haya datos, se estable
 // Obtener la referencia al contenedor de datos en la página
 const datosContainer = document.getElementById('datos-container');
 
+
+
 // Crear una plantilla de cadena HTML para mostrar los datos
 const datosHTML = `
-  <h2>Últimas 5 noticias:</h2>
+  <h2>Últimas noticias:</h2>
   <ul>
     ${datos.slice(-5).map(dato => `
     <br>
+    <li><h1><a href="./Noticia${enlace}">${dato.titulo}</a></h1></li>
     <li><b>Fecha:</b> ${dato.fecha}</li>  
     <li><b>Categoria:</b> ${dato.categoria}</li>
     <li><b>Descripcion:</b> ${dato.descripcion}</li>
     <li><b>Autor:</b> ${dato.autor}</li>
-    <li><b>Titulo:</b> ${dato.titulo}</li>
+    
     `).join('')}
   </ul>
 `;
